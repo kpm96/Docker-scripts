d0ba417f69d58855b39da46966bd5f51ba5b4454
Krzysztof Milewski <42498519+kpm96@users.noreply.github.com>
2022-02-13 21:27:07 +0100

    Add files via upload

---
+++ pihole_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo docker run -d \
+	--name pihole \
+	-p 53:53/tcp \
+	-p 53:53/udp \
+	-p 80:80/tcp \
+	-p 443:443 \
+	-v "$(pwd)/etc-pihole/:/etc/pihole/" \
+	-v "$(pwd)/etc-dnsmasq.d/:/etc/dnsmasq.d/" \
+	-e TZ="Europe/Warsaw" \
+	--dns=127.0.0.1 --dns=9.9.9.9 \
+	--restart=unless-stopped \
+	pihole/pihole:latest
+
+sudo docker exec -it pihole pihole -a -p
